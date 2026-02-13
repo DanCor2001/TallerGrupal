18de9c71f7c2c44fddf6c103c8567782399253f8
Ana Ruth Zam <anaruthzam@gmail.com>
2026-02-13 17:18:09 -0500

    Calculadora descuentos

---
+++ excercises/ejercicio7.py
@@ -0,0 +1,19 @@
+class CalculadoraDescuentos:
+    def __init__(self, precio, porcentaje):
+        self.precio = precio
+        self.porcentaje = porcentaje
+        
+    def montoDescuento(self):
+        descuento = self.precio * (self.porcentaje/ 100)
+        return descuento
+    
+    def descuentoFinal(self):
+        return self.precio - self.montoDescuento()
+    
+precio = float(input("Ingrese el precio del producto: "))
+porcentaje = float(input("Ingrese el porcenjate del descuento: "))   
+
+calculadora = CalculadoraDescuentos(precio, porcentaje)
+
+print("Monto del descuento:", calculadora.montoDescuento())
+print("Monto del precio final:", calculadora.descuentoFinal())
